2ca9a719ff89ff7d92a3c6ac999f97c888ebfb89
JFL110 <typus24@hotmail.com>
2020-03-30 10:55:36 +0200

    Add resoure loader inputstream method

--- src/main/java/org/jfl110/app/ResourceLoader.java
+++ src/main/java/org/jfl110/app/ResourceLoader.java
@@ -1,5 +1,6 @@
 package org.jfl110.app;
 
+import java.io.InputStream;
 import java.util.Optional;
 
 import com.google.inject.ImplementedBy;
@@ -11,9 +12,16 @@ import com.google.inject.ImplementedBy;
 public interface ResourceLoader {
 
 	/**
+	 * @return an InputStream for the specified resource, or throw an exception if it doesn't exist.
+	 */
+	InputStream loadAsInputStream(String resourceName);
+	
+	
+	/**
 	 * @return the contents of the specified resource as text, or throw an exception if it doesn't exist.
 	 */
 	String loadAsString(String resourceName);
+	
 	
 	/**
 	 * @return the contents of the specified resource as text, or Optional.empty() if it doesn't exist.
--- src/test/java/org/jfl110/app/TestResourceLoaderImpl.java
+++ src/test/java/org/jfl110/app/TestResourceLoaderImpl.java
@@ -3,6 +3,8 @@ package org.jfl110.app;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 
+import java.util.Scanner;
+
 import org.junit.Test;
 
 /**
@@ -12,54 +14,65 @@ import org.junit.Test;
  *
  */
 public class TestResourceLoaderImpl {
-	
+
 	private final ResourceLoader loader = new ResourceLoaderImpl(new Logger.ConsoleLogger());
 
-	
 	@Test
 	public void testResourceNotFoundOptional() {
 		assertFalse(loader.loadAsStringIfExists("NON EXISTENT RESOURCE").isPresent());
 	}
-	
-	
+
+
 	@Test(expected = IllegalArgumentException.class)
 	public void testResourceNotFound() {
 		loader.loadAsString("NON EXISTENT RESOURCE");
 	}
-	
-	
+
+
 	@Test
 	public void testResourceBadNameOptional() {
 		assertFalse(loader.loadAsStringIfExists("../#34543'<").isPresent());
 	}
-	
-	
+
+
 	@Test(expected = IllegalArgumentException.class)
 	public void testResourceBadName() {
 		loader.loadAsString("../#34543'<");
 	}
 	
 	
+	@Test(expected = IllegalArgumentException.class)
+	public void testBadResourceInputStream() {
+		loader.loadAsInputStream("../#34543'<");
+	}
+
 
 	@Test(expected = IllegalArgumentException.class)
 	public void testResourceEmptyName() {
 		loader.loadAsString("");
 	}
-	
-	
+
+
 	@Test
 	public void testResourceEmptyNameOptional() {
 		assertFalse(loader.loadAsStringIfExists("NON EXISTENT RESOURCE").isPresent());
-
 	}
-	
-	
+
+
 	@Test
 	public void testResource() {
 		assertEquals("Expected Content", loader.loadAsString("test-resource-one.txt"));
 		assertEquals("Expected Content", loader.loadAsStringIfExists("test-resource-one.txt").orElseThrow(AssertionError::new));
-		
+
 		assertEquals("Expected Content", loader.loadAsString("./test-resource-one.txt"));
 		assertEquals("Expected Content", loader.loadAsStringIfExists("./test-resource-one.txt").orElseThrow(AssertionError::new));
+
+		try (Scanner scanner = new Scanner(loader.loadAsInputStream("./test-resource-one.txt"))) {
+			StringBuffer sb = new StringBuffer();
+			while (scanner.hasNext()) {
+				sb.append(scanner.nextLine());
+			}
+			assertEquals("Expected Content", sb.toString());
+		}
 	}
 }
